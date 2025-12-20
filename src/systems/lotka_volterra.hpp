83b67023ae2de210f18a2ec01eace8e7575066cd
Cristóbal Armaza <carmazab@gmail.com>
2025-12-19 23:25:27 -0600

    remove derived state

--- src/systems/lotka_volterra.hpp
+++ src/systems/lotka_volterra.hpp
@@ -6,30 +6,22 @@
 #include "core/types.hpp"
 
 namespace systems {
-struct LotkaVolterraState : public core::State<2> {
-  constexpr LotkaVolterraState(double x, double y) noexcept
-      : core::State<2>{x, y} {}
-
-  constexpr double x() const noexcept { return data_[0]; }
-  constexpr double y() const noexcept { return data_[1]; }
-};
-
 struct LotkaVolterra {
-  using state_t = LotkaVolterraState;
+  using state_t = core::State<2>;
 
-  static constexpr std::size_t dof = 2;
+  static constexpr core::index_t dof = state_t::dof;
 
   constexpr LotkaVolterra(double a, double b, double c, double d) noexcept
       : a_{a}, b_{b}, c_{c}, d_{d} {}
 
   constexpr state_t dudt(double t, const state_t& state) const noexcept {
-    return state_t{state.x() * (a_ - b_ * state.y()),
-                   state.y() * (d_ * state.x() - c_)};
+    return state_t{state[0] * (a_ - b_ * state[1]),
+                   state[1] * (d_ * state[0] - c_)};
   }
 
   double hamiltonian(const state_t& state) const {
-    return d_ * state.x() - c_ * std::log(state.x()) + b_ * state.y() -
-           a_ * std::log(state.y());
+    return d_ * state[0] - c_ * std::log(state[0]) + b_ * state[1] -
+           a_ * std::log(state[1]);
   }
 
  private:
