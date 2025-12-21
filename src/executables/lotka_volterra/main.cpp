6409ada3765be3ebbc69f5f986c1479d10c55721
Cristóbal Armaza <carmazab@gmail.com>
2025-12-20 23:57:46 -0600

    observe time and state

--- src/executables/lotka_volterra/main.cpp
+++ src/executables/lotka_volterra/main.cpp
@@ -1,10 +1,13 @@
+#include "io/h5_writer.hpp"
 #include "numalg/timesteppers/runge_kutta_4.hpp"
 #include "systems/lotka_volterra.hpp"
 
 int main() {
   using system_t = systems::LotkaVolterra;
+  using state_t = system_t::state_t;
 
   const system_t system{0.05, 0.01, 0.03, 0.005};
+  io::H5Writer<state_t> writer{"lotka_volterra.h5"};
 
   const double dt{0.1};
   numalg::timesteppers::RungeKutta4 stepper{dt};
