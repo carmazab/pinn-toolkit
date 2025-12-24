cf07821a4389a7e9cb54a1a364a3549411108cc8
Cristóbal Armaza <carmazab@gmail.com>
2025-12-24 06:17:17 -0600

    add option-like concept

--- src/io/concepts.hpp
+++ src/io/concepts.hpp
@@ -1,10 +1,18 @@
 #pragma once
 
 #include <concepts>
+#include <string>
 
 #include "core/concepts.hpp"
 
 namespace io {
+template <typename O>
+concept OptionLike = requires(O o) {
+  typename O::type;
+  { o.value } -> std::same_as<typename O::type&>;
+  { O::name() } -> std::same_as<std::string>;
+};
+
 template <typename W, typename S>
 concept WriterLike =
     core::StateLike<S> && requires(W w, typename S::data_t t, const S& s) {
