8b1e86e87a53805878320a64b0c283d78f1d8b20
Cristóbal Armaza <carmazab@gmail.com>
2025-12-26 19:21:29 -0600

    modernize cmake build

--- cmake/AddUnitTestExecutable.cmake
+++ cmake/AddUnitTestExecutable.cmake
@@ -1,23 +1,40 @@
 include(CTest)
 include(Catch)
 
-function(add_unit_test_executable TEST_LIBRARY SOURCE_FILES)
-  add_executable(
-    ${TEST_LIBRARY}
-    ${SOURCE_FILES}
+function(add_unit_test_executable LIBRARY)
+
+  set(TEST_LIBRARY test_${LIBRARY})
+
+  cmake_parse_arguments(
+    ARG
+    ""
+    ""
+    "SOURCES;DEPENDENCIES"
+    ${ARGN}
+  )
+
+  if(NOT ARG_SOURCES)
+    message(FATAL_ERROR
+      "add_unit_test_executable(${LIBRARY}): no SOURCES specified"
     )
+  endif()
+
+  add_executable(${TEST_LIBRARY})
+
+  target_sources(
+    ${TEST_LIBRARY}
+    PRIVATE
+    ${ARG_SOURCES}
+  )
 
   target_link_libraries(
     ${TEST_LIBRARY}
     PRIVATE
+    ${LIBRARY}
     testing
-    )
-
-  target_include_directories(
-    ${TEST_LIBRARY}
-    PRIVATE
-    "${CMAKE_SOURCE_DIR}/tests/unit"
-    )
+    ${ARG_DEPENDENCIES}
+  )
 
   catch_discover_tests(${TEST_LIBRARY})
+
 endfunction()
