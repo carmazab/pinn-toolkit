72e69ab6230c0f60ba6d3e5b5f6cac7a4792ff0b
Cristóbal Armaza <carmazab@gmail.com>
2026-02-06 23:43:32 -0600

    wrap logic error throw check

--- tests/unit/testing/check_exception.hpp
+++ tests/unit/testing/check_exception.hpp
@@ -1,10 +1,17 @@
 #pragma once
 
+#include <stdexcept>
+
 #include <catch2/catch_test_macros.hpp>
 
 namespace testing {
 template <class Exception, class F>
 void check_throws_as(F&& f) {
   CHECK_THROWS_AS(f(), Exception);
+}
+
+template <class F>
+void check_throws_as_logic_error(F&& f) {
+  check_throws_as<std::logic_error>(f);
 }
 }  // namespace testing
